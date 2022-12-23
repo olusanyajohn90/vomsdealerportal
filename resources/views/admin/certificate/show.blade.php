0e627b770384a467242b7ef4f1a04bfe4a3279c2
olusanyajohn90 <olusanya.john90@gmail.com>
2022-12-23 20:16:54 +0100

    updated cert

--- resources/views/admin/certificate/show.blade.php
+++ resources/views/admin/certificate/show.blade.php
@@ -66,7 +66,7 @@ $userstateid = Auth::user()->state_id;
                             <div class="col-md-6 col-12">
                                 <div class="mb-1">
                                     <label class="form-label" for="first-name-column">State</label>
-                                    <br> <strong> {{ $state->name }} </strong>
+                                    <br> <strong> {{ $certificate->state }} </strong>
                                 </div>
                             </div>
 
